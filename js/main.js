24491f7a6973267b1d9240a26fc5fde46c2d7901
BibianaDavila <00265176@ufrgs.br>
2018-07-25 15:15:25 -0300

    js animations

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@ function positionBg(){
 	var scroll = $(document).scrollTop();
 	$('.overlay-bg').css('background-position-y',scroll/4);
 	$('.overlay-bg.dois').css('background-position-y',scroll/3);
+	var height = 100-scroll/3;
+	$('#home article').css('min-height',height+'vh');
+	$('.earth').css('marginTop',scroll/2);
+	$('.earth').css('marginLeft',scroll);
+
+
+	console.log('wait');
+	var offset = $('#dois').offset();
+	var scroll2 = scroll - offset.top;
+	$('.fish').css('marginTop',0-scroll2/3);
 }
 
 $(document).ready(function(){
