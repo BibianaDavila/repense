967f795c47c025f8d94aec5a566a3d79c8c50b1e
BibianaDavila <00265176@ufrgs.br>
2018-07-25 21:06:14 -0300

    yep

--- index.php
+++ index.php
@@ -14,14 +14,12 @@
 
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 
-	<link rel="stylesheet" href="css/style2.css">
 	<link rel="stylesheet" href="css/style.css">
 
 </head>
 
 <body>	
 	<div class="site-wrapper">
-		
 		<section id="home">
 			<div class="overlay-bg"></div>
 			<div class="overlay-bg dois"></div>
@@ -32,29 +30,163 @@
 			</article>
 		</section>
 		<section class="content" id="prim">
-			<div class="container">
-				<div class="bubble-img">
-					<div class="text-center">	
-						<img src="img/earth.png" class="earth">
+			<article>
+				<div class="container">
+					<div class="bubble-img">
+						<div class="text-center">	
+							<img src="img/earth.png" class="earth">
+						</div>
+					</div>
+					<div class="bubble" id="um">
+						<div class="bubble-text">
+
+							<h2>POUCA TERRA PARA TANTO PLÁSTICO</h2>
+
+							<hr/>
+
+							<p>A rotina é corrida, o tempo é curto, você vive à base de café? Tudo bem, a gente te entende.</p>
+
+							<p>Mas sabe aquele copo descartável? O canudinho do refri? A garrafinha PET?</p>
+
+							<p>Todo esse plástico não desaparece num passe de mágica. O lixo que criamos <b>não é descartável</b>.</p>
+
+						</div>
 					</div>
 				</div>
-				<div class="bubble" id="um">
+			</article>
+		</section>
+		<section class="content" id="seg">
+			<div class="container">
+				<div class="bubble" id="dois">
 					<div class="bubble-text">
 
-						<h2>POUCA TERRA PARA TANTO PLÁSTICO</h2>
+						<h2>CAIU NA REDE?<br/>É PLÁSTICO!</h2>
 
 						<hr/>
 
-						<p>A rotina é corrida, o tempo é curto, você vive à base de café? Tudo bem, a gente te entende.</p>
+						<p>De acordo com a ONU Meio Ambiente, os oceanos recebem, anualmente, ao menos <b>8 milhões de toneladas de plástico</b>.</p>
 
-						<p>Mas sabe aquele copo descartável? O canudinho do refri? A garrafinha PET?</p>
+						<p>É como se jogassemos 250kg de lixo por segundo em nossas águas.</p>
 
-						<p>Todo esse plástico não desaparece num passe de mágica. O lixo que criamos <b>não é descartável</b>.</p>
+						<p>Para se ter uma ideia, pesquisas apontam que até 2050 os mares vão ter <b>mais plástico do que peixes</b>.</p>
+
+					</div>
+				</div>
+				<div class="bubble-img">
+					<div class="text-center">	
+						<img src="img/fish.png" class="fish">
+					</div>
+				</div>
+				<div class="bubble-img2">
+					<div class="text-center">	
+						<img src="img/fish.png" class="fish2">
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="content" id="ter">
+			<article>
+				<div class="container">
+
+					<div class="content-center">
+						<div class="title text-center">
+							<h1>CHEGOU A HORA DE <br/>REPENSAR NOSSOS HÁBITOS</h1>
+						</div>
+					</div>
+
+				</div>
+			</article>
+		</section>
+		<section class="content" id="sex">
+			<div class="container">
+				<div class="bubble" id="seis">
+					<div class="bubble-text">
+
+						<h2>CUIDE DE SEU LIXO</h2>
+
+						<hr/>
+
+						<p>Na hora de descartar materias, tenha em mente que existem dois tipos de lixo.</p>
+
+						<p><h5>LIXO SECO:</h5> Tudo que pode ser reciclado, como plástico, papel, vidro, alumínio. </p>
+
+						<p><h5>LIXO MOLHADO:</h5> O que não pode ser reaproveitado, como resíduos orgânicos e papel sujo ou molhado.</p>
 
 					</div>
 				</div>
 			</div>
 		</section>
+		<section class="content" id="qui">
+			<div class="container">
+				<div class="bubble" id="cinco">
+					<div class="bubble-text">
 
+						<h2>DIGA ADEUS AO PET</h2>
+
+						<hr/>
+
+						<p>Apesar do PET ser reciclável, pesquisas mostram que, no Brasil, ao menos 50% das garrafas não são recicladas.</p>
+
+						<p>Na hora de comprar bebidas, procure consumir <b>latas de alumínio</b>. A taxa de reciclagem das latinhas chega a 98% no país!</p>
+
+						<p>Já na hora de beber água, prefira ter sua própria garrafa em mãos. </p>
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="content" id="quar">
+			<div class="container">
+				<div class="bubble-img-container">
+					<div class="bubble-img">
+						<div class="text-center">	
+							<img src="img/coffee.png" class="coffee">
+						</div>
+					</div>
+					<div class="bubble-img2">
+						<div class="text-center">	
+							<img src="img/coffee.png" class="coffee2">
+						</div>
+					</div>
+				</div>
+				<div class="bubble" id="quatro">
+					<div class="bubble-text">
+
+						<h2>ADOTE UMA CANECA</h2>
+
+						<hr/>
+
+						<p>Melhor do que reciclar ou reutilizar o plástico, é <b>não consumí-lo</b> em primeiro lugar.</p>
+
+						<p>Ações simples como levar uma caneca na mochila tornam seu dia a dia mais sustentável.</p>
+
+						<p>Na FABICO, os bares aceitam servir qualquer bebida diretamente em seu copo. É fácil fazer sua parte.</p>
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="content" id="set">
+			<article>
+				<div class="container">
+
+					<div class="content-center">
+						<img src="img/gota.png" class="drop">
+						<div class="title text-center">
+							<h1>NÃO ESPERE ATÉ A <br class="hide-xs" />ÚLTIMA GOTA D'ÁGUA</h1>
+							<h2>Quando todos paramos para pensar,<br class="hide-xs"/> o planeta inteiro vence</h2>
+						</div>
+					</div>
+
+				</div>
+			</article>
+		</section>
 	</div>
+	<footer>
+		<div class="content-center">
+			<div class="title text-center">
+				<h4>Porto Alegre, julho de 2018</h4>
+			</div>
+		</div>
+	</footer>
 </body>
--- js/main.js
+++ js/main.js
@@ -1,24 +1,71 @@
-function positionBg(){
+function animation(){
 	var scroll = $(document).scrollTop();
 	$('.overlay-bg').css('background-position-y',scroll/4);
 	$('.overlay-bg.dois').css('background-position-y',scroll/3);
+
 	var height = 100-scroll/3;
-	$('#home article').css('min-height',height+'vh');
-	$('.earth').css('marginTop',scroll/2);
-	$('.earth').css('marginLeft',scroll);
+	//$('#home article').css('min-height',height+'vh');
 
+	var sec1 = $('#prim').offset().top;
+	var sec2 = $('#seg').offset().top;
+	var sec3 = $('#ter').offset().top;
+	var sec4 = $('#sex').offset().top;
+	var sec5 = $('#qui').offset().top;
+	var sec6 = $('#quar').offset().top;
+	var sec7 = $('#set').offset().top;
 
-	console.log('wait');
-	var offset = $('#dois').offset();
-	var scroll2 = scroll - offset.top;
-	$('.fish').css('marginTop',0-scroll2/3);
+	/* home */
+	if(scroll<sec1){
+		
+	}
+	/* primeiro */
+	else if(scroll<sec2){
+		$('.earth').css('marginTop',scroll/3);
+		$('.earth').css('marginLeft',scroll/3);
+	}
+	/* segundo */
+	else if(scroll<sec3){
+		var offset = $('#dois').offset();
+		var scroll2 = scroll - offset.top;
+		$('.fish').css('marginTop',0-scroll2/4);
+		$('.fish').css('marginLeft',0-scroll2);
+		$('.fish2').css('marginTop',0-scroll2/4);
+		$('.fish2').css('marginRight',0-scroll2);
+	}
+	/* terceiro */
+	else if(scroll<sec4){
+
+	}
+	/* quarto */
+	else if(scroll<sec5){
+
+	}
+	/* quinto */
+	else if(scroll<sec6){
+
+	}
+	/* sexto */
+	else if(scroll<sec7){
+		var offset = $('#quar').offset();
+		var scroll2 = scroll - offset.top;
+		$('.coffee').css('marginTop',100-scroll2/2);
+		$('.coffee').css('marginLeft',0-scroll2/2);
+		$('.coffee2').css('marginTop',0-scroll2/2);
+		$('.coffee2').css('marginRight',100-scroll2/2);
+	}
+	/* sétimo */
+	else{
+		var offset = $('#set').offset();
+		var scroll2 = scroll - offset.top;
+		$('.drop').css('marginTop',0-scroll2);
+	}
 }
 
 $(document).ready(function(){
 
+	animation();
+
 	$(window).scroll(function(){
-
-		positionBg();
-
+		animation();
 	});
 });
